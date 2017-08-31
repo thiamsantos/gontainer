5df242ceb59c740ee47f043936a944ecd9d1f236
root <root@box-codeanywhere.com>
2017-08-31 16:03:15 -0400

    :sparkles: add filesystem

--- main.go
+++ main.go
@@ -7,10 +7,9 @@ import (
 	"syscall"
 )
 
-func main() {
-	fmt.Println("Running container")
-	
-	cmd := exec.Command("/bin/bash")
+func run(name string, args ...string) {
+	fmt.Println("Running %v", name)
+	cmd := exec.Command(name, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -21,5 +20,31 @@ func main() {
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err)
+	}
+}
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run("/proc/self/exe", append([]string{"child"}, "/bin/bash")...)
+	case "child":
+		err := syscall.Chroot("files")
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		err = os.Chdir("/")
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		err = syscall.Mount("proc", "proc", "proc", 0, "")
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		run(os.Args[2])
+	default:
+		panic("What???")
 	}
 }
